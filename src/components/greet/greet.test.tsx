919341d90b159a5fbc128bf4e5feaf9ccc4e7c84
Raxit <raxitpatel553@gmail.com>
2023-05-05 11:05:27 +0530

    Grouping

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,15 +1,17 @@
 import {render,screen} from '@testing-library/react'
 import { Greet } from './greet'
 
-test('Greet render correctly',()=>{
-    render(<Greet/>)
-    const textElement = screen.getByText(/hello/i)
-    expect(textElement).toBeInTheDocument();
-})
-
-test('Greet render with name',()=>{
-    render(<Greet name='Vishwas'/>)
-    const textElement = screen.getByText("Hello Vishwas")
-    expect(textElement).toBeInTheDocument();
+describe('Greet', ()=>{
+    test('Greet render correctly',()=>{
+        render(<Greet/>)
+        const textElement = screen.getByText(/hello/i)
+        expect(textElement).toBeInTheDocument();
+    })
+    
+    test('Greet render with name',()=>{
+        render(<Greet name='Vishwas'/>)
+        const textElement = screen.getByText("Hello Vishwas")
+        expect(textElement).toBeInTheDocument();
+    })    
 })
 
